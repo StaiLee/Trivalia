168c386bf0b0edab5b64f52b4fff2d4eb473caf0
dre7djib <djibril.camara.pradier@gmail.com>
2024-02-05 22:23:52 +0100

    Update About Page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,346 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<!doctype html>
+<html class="h-100" lang="en">
 
-<h1>About</h1>
-    
-</body>
+  <head>
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="description" content="About Page">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon.png">
+  <meta name="generator" content="Eleventy v2.0.0">
+  <meta name="HandheldFriendly" content="true">
+  <title>About Trivalia</title>
+  <link href="{{ asset('/import/assets/css/styles.css') }}" rel="stylesheet"/>
+  <link rel="stylesheet" href="{{ asset('/import/assets/css/effetunderline.css') }}">
+
+   <style>
+
+/* inter-300 - latin */
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('fonts/inter-v12-latin-300.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('fonts/inter-v12-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+/* inter-400 - latin */
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('fonts/inter-v12-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('fonts/inter-v12-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: local(''),
+       url('fonts/inter-v12-latin-500.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('fonts/inter-v12-latin-500.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: local(''),
+       url('fonts/inter-v12-latin-700.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+       url('fonts/inter-v12-latin-700.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+}
+
+</style>
+
+
+  </head>
+
+  <body class="bg-black text-white " data-bs-spy="scroll" data-bs-target="#navScroll">
+
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="margin-bottom: 2rem;" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Trivalia</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+    </nav>
+
+
+
+    <main>
+      <div class="w-100 overflow-hidden position-relative bg-black text-white"  data-aos="fade">
+  <div class="position-absolute w-100 h-100 bg-black opacity-75 top-0 start-0"></div>
+  <div class="container py-vh-4 position-relative mt-5 px-vw-5 text-center">
+  <div class="row d-flex align-items-center justify-content-center py-vh-5">
+    <div class="col-12 col-xl-10">
+      <span class="h5 text-secondary fw-lighter">Our Project</span>
+      <h1 class="display-huge mt-3 mb-3 lh-1">We connect all of your photos at the same place</h1>
+    </div>
+    <div class="col-12 text-center">
+      <a href="access.html" class="btn btn-xl btn-light" style="background-color: #ccff33;">Join us
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+</svg>
+</a>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="w-100 position-relative bg-black text-white bg-cover d-flex align-items-center mt-3">
+  <div class="container-fluid px-vw-5">
+    <div class="position-absolute w-100 h-50 bg-dark bottom-0 start-0"></div>
+    <div class="row d-flex align-items-center position-relative justify-content-center px-0 g-5">
+      <div class="col-12 col-lg-6">
+        <img src="{{ asset('/import/assets/pic/img_about/barrage.jpg') }}" width="2280" height="1732" alt="abstract image" class="img-fluid position-relative rounded-5 shadow" data-aos="fade-up">
+      </div>
+      <div class="col-12 col-md-6 col-lg-3">
+        <img src="{{ asset('/import/assets/pic/img_about/train.jpg') }}" width="1116" height="1578" alt="abstract image" class="img-fluid position-relative rounded-5 shadow" data-aos="fade-up" data-aos-duration="2000">
+      </div>
+      <div class="col-12 col-md-6 col-lg-3">
+        <img src="{{ asset('/import/assets/pic/img_about/tree.jpg') }}" width="1116" height="848" alt="abstract image" class="img-fluid position-relative rounded-5 shadow" data-aos="fade-up" data-aos-duration="3000">
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-dark mt-3">
+  <div class="container px-vw-5 py-vh-5">
+    <div class="row d-flex align-items-center">
+      <div class="col-12 col-lg-7 text-lg-end" data-aos="fade-right">
+        <span class="h5 text-secondary fw-lighter">What we do ?</span>
+        <h2 class="display-4">Krid is a specialized company dedicated to capturing the immersive beauty of nature through breathtaking landscape photography.</h2>
+      </div>
+      <div class="col-12 col-lg-5" data-aos="fade-left">
+        <h3 class="pt-5">Our Team</h3>
+        <p class="text-secondary"> Our team of passionate photographers is committed to exploring the wonders of nature and immortalizing them with artistic precision. <br>
+          <a href="#" class="link-fancy link-fancy-light me-2" style="color: #ccff33;">Tell me more</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+</svg>
+</p>
+        <h3 class="border-top border-secondary pt-5 mt-5">Where ?</h3>
+        <p class="text-secondary">Whether it's majestic mountains, serene rivers, vast plains, or picturesque sunsets, we strive to capture the grandeur of nature in every shot. Equipped with cutting-edge gear and a deep understanding of light and composition, we create landscape photographs that evoke wonder and a connection with the natural world..<br>
+          <a href="#" class="link-fancy link-fancy-light me-2" style="color: #ccff33;">Tell me more</a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+</svg>
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-black py-vh-3 mt-3">
+  <div class="container bg-black px-vw-5 py-vh-3 rounded-5 shadow">
+
+  <div class="row gx-5">
+    <div class="col-12 col-md-6">
+      <div class="card bg-transparent mb-5" data-aos="zoom-in-up">
+        <div class="bg-dark shadow rounded-5 p-0">
+          <img src="{{ asset('/import/assets/pic/img_about/window.jpg') }}" width="582" height="327" alt="window image" class="img-fluid rounded-5 no-bottom-radius" loading="lazy">
+          <div class="p-5">
+            <h2 class="fw-lighter">Carlo Obrian</h2>
+            <p class="pb-4 text-secondary">Shibuya City, Tokyo, Japan</p>
+            <a href="#" class="link-fancy link-fancy-light" style="color: #ccff33;">Read more</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-transparent" data-aos="zoom-in-up">
+        <div class="bg-dark shadow rounded-5 p-0">
+          <img src="{{ asset('/import/assets/pic/img_about/australia.jpg') }}" width="582" height="442" alt="new-york image" class="img-fluid rounded-5 no-bottom-radius" loading="lazy">
+          <div class="p-5">
+            <h2 class="fw-lighter">Josh Withers</h2>
+            <p class="pb-4 text-secondary">Currumbin, QLD, Australia</p>
+            <a href="#" class="link-fancy link-fancy-light" style="color: #ccff33;">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 mt-3" >
+      <div class="p-5 pt-0 mt-5" data-aos="fade">
+        <span class="h5 text-secondary fw-lighter">Take a look to</span>
+        <h2 class="display-4">Our Photos</h2>
+      </div>
+      <div class="card bg-transparent mb-5 mt-5" data-aos="zoom-in-up">
+        <div class="bg-dark shadow rounded-5 p-0">
+          <img src="{{ asset('/import/assets/pic/img_about/ski.jpg') }}" width="582" height="390" alt="skier image" class="img-fluid rounded-5 no-bottom-radius" loading="lazy">
+          <div class="p-5">
+            <h2 class="fw-lighter">Mateo Milan</h2>
+            <p class="pb-4 text-secondary">Somewhere</p>
+            <a href="#" class="link-fancy link-fancy-light" style="color: #ccff33;">Read more</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-transparent" data-aos="zoom-in-up">
+        <div class="bg-dark shadow rounded-5 p-0">
+          <img src="{{ asset('/import/assets/pic/img_about/madera.jpg') }}" width="582" height="327" alt="abstract image" class="img-fluid rounded-5 no-bottom-radius" loading="lazy">
+          <div class="p-5">
+            <h2 class="fw-lighter">Ferdinando Grimaldi</h2>
+            <p class="pb-4 text-secondary">Madera, Portogallo</p>
+            <a href="#" class="link-fancy link-fancy-light" style="color: #ccff33;">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+
+<div class="bg-dark mt-3">
+<div class="container px-vw-5 py-vh-5">
+  <div class="row d-flex align-items-center">
+    <div class="col-12 col-lg-5 text-center text-lg-end" data-aos="zoom-in-right">
+      <span class="h5 text-secondary fw-lighter">Use our photos</span>
+      <h2 class="display-4">You can get all of our photos for a single price</h2>
+    </div>
+    <div class="col-12 col-lg-7 bg-dark rounded-5 py-vh-3 text-center my-5" data-aos="zoom-in-up">
+      <h2 class="display-huge mb-5">
+        <span class="fs-4 me-2 fw-light">$</span><span class="border-bottom border-5" style="color: #ccff33;">200€</span><span class="fs-6 fw-light">/year</span></h2>
+      <p class="lead text-secondary">Order now to bring a touch of wonder and sophistication to your living space. At Krid, we transform photography into a captivating visual experience, offering a constant invitation to explore and appreciate the world around us.</p>
+      <a href="#" class="btn btn-xl btn-light">Sign up
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+</svg>
+</a>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+    </main>
+
+    <footer class="bg-black border-top border-dark mt-3">
+  <div class="container py-vh-4 text-secondary fw-lighter">
+    <div class="row">
+      <div class="col-12 col-lg-5 py-4 text-center text-lg-start">
+            <a class="navbar-brand pe-md-4 fs-4 col-12 col-md-auto text-center" href="index.html">
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-stack" viewBox="0 0 16 16">
+    <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.598.598 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.598.598 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.598.598 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535L7.733.063z"/>
+    <path d="m14.12 6.576 1.715.858c.22.11.22.424 0 .534l-7.568 3.784a.598.598 0 0 1-.534 0L.165 7.968a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.659z"/>
+  </svg>
+  <span class="ms-md-1 mt-1 fw-bolder me-md-5">Krid</span>
+</a>
+
+      </div>
+      <div class="col border-end border-dark">
+        <span class="h6">Company</span>
+<ul class="nav flex-column">
+  <li class="nav-item">
+    <a href="#" class="link-fancy link-fancy-light">About us</a>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="link-fancy link-fancy-light">Legal</a>
+  </li>
+
+  <li class="nav-item">
+    <a href="#" class="link-fancy link-fancy-light">Career</a>
+  </li>
+  <li class="nav-item">
+    <a href="#" class="link-fancy link-fancy-light">Contact</a>
+  </li>
+</ul>
+      </div>
+      <div class="col border-end border-dark">
+                <span class="h6">Services</span>
+                <ul class="nav flex-column">
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Products</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Customers</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Success Stories</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">More</a>
+                  </li>
+                </ul>
+      </div>
+      <div class="col">
+                <span class="h6">Support</span>
+                <ul class="nav flex-column">
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">About us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Legal</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Career</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="link-fancy link-fancy-light">Contact</a>
+                  </li>
+                </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/aos.js"></script>
+<script>
+AOS.init({
+ duration: 800, // values from 0 to 3000, with step 50ms
+});
+</script>
+<script>
+  let scrollpos = window.scrollY
+  const header = document.querySelector(".navbar")
+  const header_height = header.offsetHeight
+
+  const add_class_on_scroll = () => header.classList.add("scrolled", "shadow-sm")
+  const remove_class_on_scroll = () => header.classList.remove("scrolled", "shadow-sm")
+
+  window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+    console.log(scrollpos)
+  })
+</script>
+
+  </body>
 </html>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -18,14 +18,13 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Trivalia</a>
+                <a class="navbar-brand" href="{{ url('/') }}">Trivalia</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
                         <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
