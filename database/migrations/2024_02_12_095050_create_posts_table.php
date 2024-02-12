f2aed19d5c72545ea8488c6308e80daf13700450
StaiLee <ilyesstaili@gmail.com>
2024-02-12 10:56:16 +0100

    creation table post

---
+++ database/migrations/2024_02_12_095050_create_posts_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
