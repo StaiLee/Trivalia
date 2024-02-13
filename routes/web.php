1149cad2c61ed7f8af8457983a094a5b0e040d6a
StaiLee <ilyesstaili@gmail.com>
2024-02-13 09:46:18 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Inertia\Inertia;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| which contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -71,15 +71,12 @@ Route::post('/checkout', function (Request $request) {
     return 'Payment successful!';
 });
 
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
  
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route:
 });
-
-require __DIR__.'/auth.php';
-
