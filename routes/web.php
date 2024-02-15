f7d05d60f312338db1ea7bb660f7dfb0be98c72d
dre7djib <djibril.camara.pradier@gmail.com>
2024-02-16 00:25:53 +0100

    Fix Routes Products

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.upda
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 // Products
-Route::get('/p', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
