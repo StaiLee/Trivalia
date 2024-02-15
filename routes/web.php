52a2b68646342e9f29363edf86544056b781078f
dre7djib <djibril.camara.pradier@gmail.com>
2024-02-15 11:58:04 +0100

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -40,17 +40,11 @@ Route::get('/home', function () {
     return view('homepage');
 });
 
-// Checkout
-Route::get('/checkout', function () {
-    return view('checkout');
-});
-
 Route::controller(StripePaymentController::class)->group(function(){
     Route::get('stripe', 'stripe')->name('stripe.index');
     Route::get('stripe/checkout', 'stripeCheckout')->name('stripe.checkout');
     Route::get('stripe/checkout/success', 'stripeCheckoutSuccess')->name('stripe.checkout.success');
 });
-
 
 
 Route::get('/dashboard', function () {
